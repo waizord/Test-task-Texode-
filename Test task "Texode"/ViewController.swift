96a9880686f41128d8e7ca806877f4e5a67e43dc
Ivan <waizord@yandex.ru>
2020-08-12 10:07:00 +0300

    Initial Commit

---
+++ Test task "Texode"/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test task "Texode"
+//
+//  Created by Ivan on 8/12/20.
+//  Copyright © 2020 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
